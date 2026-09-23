d888d041a65d6e4b252e77fa0fec805db36d41cb
tinurk <reparxa@gmail.com>
2026-09-23 16:09:22 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+       AsyncSession,
+       async_sessionmaker,
+       create_async_engine,
+   )
 from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.db"
